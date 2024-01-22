bc012ea5a040cf1a50366596055593895abd54f7
Steven Roscoe <112785884+StevenRoscoe@users.noreply.github.com>
2024-01-22 13:49:54 -0600

    add user script

---
+++ add-user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#will add the username that was input
+sudo useradd steve123
+
+#allows creation of a password for username
+sudo passwd steve123
+
+#will display the account created message
+echo "Account Created!"
